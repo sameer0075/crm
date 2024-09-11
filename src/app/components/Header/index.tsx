b17110d47013b336331820d198a08988d3a1a5dc
sameer0075 <muhammad.sameer@novatoresols.com>
2024-09-11 17:16:26 +1100

    Handle Logout

--- src/app/components/Header/index.tsx
+++ src/app/components/Header/index.tsx
@@ -1,14 +1,48 @@
 'use client';
-import { useState, ChangeEvent } from 'react';
+import { useState, ChangeEvent, useRef, useEffect } from 'react';
 import { UserCircle, Search } from 'lucide-react'; // Importing the Search icon
 import Input from '../Input';
 import Subheader from '../SubHeader';
 
 export default function Header() {
   const [search, setSearch] = useState('');
+  const [isOpen, setIsOpen] = useState(false);
+  const dropdownRef = useRef(null);
+  const buttonRef = useRef(null);
+
+  const handleClick = () => {
+    setIsOpen(!isOpen);
+  };
+
+  // Close the dropdown menu if a click is detected outside of it
+  const handleClickOutside = (event: React.ChangeEvent<HTMLInputElement>) => {
+    if (
+      dropdownRef.current &&
+      !dropdownRef.current.contains(event.target) &&
+      buttonRef.current &&
+      !buttonRef.current.contains(event.target)
+    ) {
+      setIsOpen(false);
+    }
+  };
+
+  useEffect(() => {
+    // Add event listener for clicks outside
+    document.addEventListener('mousedown', handleClickOutside);
+
+    // Clean up event listener on component unmount
+    return () => {
+      document.removeEventListener('mousedown', handleClickOutside);
+    };
+  }, []);
 
   const handleSearch = (e: ChangeEvent<HTMLInputElement>) => {
     setSearch(e.target.value);
+  };
+
+  const handleLogout = () => {
+    sessionStorage.removeItem('token');
+    window.location.href = '/';
   };
 
   return (
@@ -38,7 +72,40 @@ export default function Header() {
               />
               <Search className="text-[#3673D4] absolute left-3 top-1/2 transform -translate-y-1/2 text-gray-400 h-[24px] w-[24px] opacity-80" />
             </div>
-            <UserCircle className="text-[#3673D4] opacity-80 h-6 w-6 sm:h-8 sm:w-8 text-muted-foreground" />
+            <div className="relative inline-block">
+              <div
+                ref={buttonRef}
+                onClick={handleClick}
+                className="relative inline-block text-left"
+              >
+                <div id="dropdownDelayButton" className="cursor-pointer">
+                  <UserCircle className="text-[#3673D4] opacity-80 h-6 w-6 sm:h-8 sm:w-8 text-muted-foreground" />
+                </div>
+
+                {/* Dropdown menu */}
+                {isOpen && (
+                  <div
+                    ref={dropdownRef}
+                    id="dropdownDelay"
+                    className="absolute right-0 top-[40px] z-10 bg-white divide-y divide-gray-100 rounded-lg shadow w-44 dark:bg-gray-700 group group-hover:block"
+                  >
+                    <ul
+                      className="py-2 text-sm text-gray-700 dark:text-gray-200"
+                      aria-labelledby="dropdownDelayButton"
+                    >
+                      <li className="cursor-pointer">
+                        <a
+                          onClick={handleLogout}
+                          className="block px-4 cursor-pointer py-2 hover:bg-gray-100 dark:hover:bg-gray-600 dark:hover:text-white"
+                        >
+                          Sign out
+                        </a>
+                      </li>
+                    </ul>
+                  </div>
+                )}
+              </div>
+            </div>
           </div>
         </div>
       </header>
