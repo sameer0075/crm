0d589e1e267d27e8c0f6175c0b7cfc2976861a21
sameer0075 <sameerbutt151@gmail.com>
2024-09-28 01:27:46 +0500

    fix

--- src/app/api/status/add/route.ts
+++ src/app/api/status/add/route.ts
@@ -7,15 +7,12 @@ import { composeMiddlewares } from '@/lib/middleware/middleware-composer';
 import { PrismaClient } from '@prisma/client';
 import { StatusCode } from '@/utils/enums';
 import { jwtMiddleware } from '@/lib/middleware/auth-middleware';
-import { statusSchema } from '@/utils/schemas/comment.schema';
 
 const prisma = new PrismaClient();
 
 const AddStatusHandler = async (req: NextRequest): Promise<NextResponse> => {
-  const body = await req.json();
+  const payload = await req.json();
   try {
-    const payload = statusSchema.parse(body);
-
     const data = await prisma.record_status.create({
       data: payload,
     });
